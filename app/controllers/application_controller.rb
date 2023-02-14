a0db617858a168bdc772d00d392966902eb158c9
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-02-14 23:42:30 +0100

    worc vercion. : translete

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,25 @@ class ApplicationController < ActionController::Base
     end
 #//////////////////////////////////////////////////////////////////////////////////////
     
+             #Podkluchenie perewoda
+around_action :switch_locale
+
+    private
+    
+    def switch_locale(&action)
+      locale = params[:locale] || I18n.default_locale
+      I18n.with_locale locale, &action 
+    end
+
+    def locale_form_url
+      locale = params[:locale]
+      return locale if I18n.available_locales.map(&:to_s).include?(locale)
+      nil
+    end
+    
+    def default_url_options
+    {locale:I18n.locale}
+    end
 end
           
     
