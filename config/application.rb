412b2094a2ff0fb25989a5ce10526df175803930
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-02-15 11:03:50 +0100

    work version i18n

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,7 @@ module AppBlog
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
     
-    config.i18n.available_locales = %i[en ru]
+    config.i18n.available_locales = %i[en ru pl]
     config.i18n.default_locale = :ru
     # Configuration for the application, engines, and railties goes here.
     #
