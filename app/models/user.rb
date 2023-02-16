57c3bfc35e8f20eba23467de4c218a829607cf9d
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-02-16 18:41:49 +0100

    podkluczil "user"

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :articles, through: :comments, dependent: :destroy
+         #podkluczenie "rabotaet"
+  has_many :articles, dependent: :destroy
   has_many :comments, dependent: :destroy
              
         
