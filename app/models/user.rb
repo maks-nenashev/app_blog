3eff6cd8078e7ef4dc4597f97401a90dfd699d4e
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-03-03 12:37:54 +0100

    Role

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ApplicationRecord
   enum role: { basic: 0, moderator: 1, admin: 2 }, _suffix: :role
 
   attr_accessor :old_password, :remember_token, :admin_edit
-
-  has_secure_password validations: false
+  
+  #has_secure_password validations: false #, password_digest
+  #validate :password_presence 
   #validate :password_digest
-  validate :password_presence
-  validate :correct_old_password, on: :update, if: -> { password.present? && !admin_edit }
-  validates :password, confirmation: true, allow_blank: true,
-                     length: { minimum: 6, maximum: 10 }
+  #validate :correct_old_password, on: :update, if: -> { password.present? && !admin_edit }
+  #validates :password, confirmation: true, allow_blank: true,
+                    # length: { minimum: 6, maximum: 10 }
   #validates :email, presence: true, uniqueness: true, 'valid_email_2/email': true
-  validate :password_complexity
-  validates :role, presence: true
+  #validate :password_complexity
+  #validates :role, presence: true
   #//////////////////////////////////////////////////////////////////////////////////////////
 
   devise :database_authenticatable, :registerable,
