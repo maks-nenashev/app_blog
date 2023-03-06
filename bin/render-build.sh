b8288fa487d9c5359588fa9b2d23ce6bfdb6c7c5
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-03-06 20:40:19 +0100

    redis

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,5 +5,5 @@ set -o errexit
 bundle install
 yarn install
 bundle exec rails assets:precompile RAILS_ENV=production
-bundle exec rake rails assets:clean
+#bundle exec rake rails assets:clean
 bundle exec rake rails db:migrate
