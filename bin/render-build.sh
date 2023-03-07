0c04af32e3c54fec413ad8fb1652287d6a96df47
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-03-07 13:00:32 +0100

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
 # exit on error
-#set -o errexit
-
-#bundle install
-#yarn install
-#rails assets:precompile 
-#bundle exec rake rails assets:precompile
-#bundle exec rake rails assets:clean
-
-#rake rails db:migrate
-#!/usr/bin/env bash
-# exit on error
 set -o errexit
 
 bundle install
-bundle exec rake rails assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+yarn install
+rails assets:precompile 
+rake rails assets:precompile
+rake rails assets:clean
+rake rails db:migrate
+#!/usr/bin/env bash
+# exit on error
+#set -o errexit
+
+#bundle install
+#bundle exec rake rails assets:precompile
+#bundle exec rake assets:clean
+#bundle exec rake db:migrate
