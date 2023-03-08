03240ed3dba28d241af2c8ef13ab55016b3f1eb0
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-03-08 19:28:50 +0100

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,6 @@ set -o errexit
 
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails rake assets:precompile
+bundle exec rails rake assets:clean
+bundle exec rails rake db:migrate
