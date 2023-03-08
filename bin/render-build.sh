f171e7bc9d9b529c27f9fbc0d645fca4019ab66f
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-03-08 19:11:40 +0100

    render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -6,14 +6,4 @@ bundle install
 yarn install
 rails assets:precompile 
 rails assets:clean
-#rake db:migrate
-#rake db:create #RAILS_ENV=production
-#!/usr/bin/env bash
-# exit on error
-#set -o errexit
-
-
-#bundle install
-#bundle exec rake rails assets:precompile
-#bundle exec rake assets:clean
 rake db:migrate
