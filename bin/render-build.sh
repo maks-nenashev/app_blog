9e2336a30d4f2873b54a6f7517afa88f2f9e863f
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-03-08 19:19:20 +0100

    redes

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,8 +2,8 @@
 # exit on error
 set -o errexit
 
+
 bundle install
-yarn install
-rails assets:precompile 
-rails assets:clean
-rake db:migrate
+bundle exec rake assets:precompile
+bundle exec rake assets:clean
+bundle exec rake db:migrate
