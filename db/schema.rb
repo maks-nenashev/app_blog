021503703079fe465858ae4de262a7e72cb6d7f4
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2024-07-06 17:47:39 +0200

    test

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_091557) do
   end
 
   create_table "comments", force: :cascade do |t|
+    t.string "title"
     t.text "body"
     t.integer "article_id", null: false
     t.datetime "created_at", null: false
@@ -77,6 +78,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_12_03_091557) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "full_name"
+    t.string "uid"
+    t.string "avatar_url"
+    t.string "provider"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
