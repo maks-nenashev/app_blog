962501988f3e5a8e129f819d6b0f0a64c0546e33
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2024-09-16 18:53:25 +0200

    test

--- db/migrate/20230205125304_create_comments.rb
+++ db/migrate/20230205125304_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
-      t.string :title
+      t.text :title
       t.text :body
       t.belongs_to :article, null: false, foreign_key: true
 
