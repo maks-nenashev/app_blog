248d642a9a8b5b527a8e1da56f1e4603e4bab48c
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2025-05-26 23:58:54 +0200

    added js

--- app/javascript/home.js
+++ app/javascript/home.js
@@ -1,21 +1,26 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+document.addEventListener("turbo:load", function () {
+  // Получение модального окна
+  var modal = document.getElementById("myModal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function(){
-  modal.style.display = "block";
-  modalImg.src = this.src;
-  captionText.innerHTML = this.alt;
-}
+  // Получение изображения и вставка его в модальное окно
+  var img = document.getElementById("myImg");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("caption");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+  if (img && modal && modalImg && captionText) {
+    img.onclick = function () {
+      modal.style.display = "block";
+      modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+    };
+  }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() { 
-  modal.style.display = "none";
-}
- 
+  // Получение элемента <span>, который закрывает модальное окно
+  var span = document.getElementsByClassName("close")[0];
+
+  if (span && modal) {
+    span.onclick = function () {
+      modal.style.display = "none";
+    };
+  }
+});
