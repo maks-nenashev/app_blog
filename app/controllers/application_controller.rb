dbbbf6f923149595697227c35f6fa9c32813bdb1
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2025-08-02 20:06:39 +0200

    factoring

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,41 @@
 class ApplicationController < ActionController::Base
-  include Pagy::Backend   #Podkluczenie "Pagy"
-  include ErrorHandling # Podkluczenie ERORR 404
-  include Pundit::Authorization    
-                     # gem "devase"
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  include Pagy::Backend
+  include ErrorHandling
+  include Pundit::Authorization
 
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
-      devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
-    end
-    #//////////////////////////////////////////////////////////////////////////////////////
-    
-             #Podkluchenie perewoda
-around_action :switch_locale
+  before_action :set_locale
 
-    private
-    
-    def switch_locale(&action)
-      locale = params[:locale] || I18n.default_locale
-      I18n.with_locale locale, &action 
-    end
+  # ... Devise config (оставь как есть)
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def locale_form_url
-      locale = params[:locale]
-      return locale if I18n.available_locales.map(&:to_s).include?(locale)
-      nil
-    end
-    
-    def default_url_options
-    {locale:I18n.locale}
-    end
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :avatar])
+  end
+
+  private
+
+  def set_locale
+    I18n.locale =
+      params[:locale] ||
+      session[:locale] ||
+      extract_locale_from_accept_language_header ||
+      I18n.default_locale
+    session[:locale] = I18n.locale
+  end
+
+  def extract_locale_from_accept_language_header
+    http_langs = request.env['HTTP_ACCEPT_LANGUAGE']
+    return nil unless http_langs
+
+    browser_locales = http_langs.scan(/[a-z]{2}/)
+    available = I18n.available_locales.map(&:to_s)
+    browser_locales.find { |locale| available.include?(locale) }
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
-          
-    
-      
-
-
-
