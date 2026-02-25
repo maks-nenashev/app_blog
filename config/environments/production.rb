0083882d57e683fe7ebb2880a7f817cad93abaee
Maksym <57881489+maks-nenashev@users.noreply.github.com>
2026-02-25 20:13:58 +0100

    new translate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -5,11 +5,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-
-  # Eager load code on boot. This eager loads most of Rails and
-  # your application in memory, allowing both threaded web servers
-  # and those relying on copy on write to perform better.
-  # Rake tasks automatically ignore this option for performance.
+  
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
@@ -32,7 +28,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.version = '1.0' # <--- ВАЖНО: обновляйте версию при изменении ассетов
+  
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
